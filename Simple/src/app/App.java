2ea16ea09a208c4667ad5aeed8820a922dabe27c
lva1307 <lva130780@gmail.com>
2022-07-09 23:28:28 -0300

    Changed message to 1

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -3,6 +3,6 @@ package app;
 public class App {
 
     public static void main(String[] args){
-      System.out.println("Hello world, what's happened?");
+      System.out.println("Hello world, what's happened to 1?");
     }
 }
